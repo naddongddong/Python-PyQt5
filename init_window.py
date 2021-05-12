c8a920b4a21b846d27bbae6d47f5ba23ac1cb11e
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-05-12 16:20:05 +0900

    Add files via upload

--- init_window.py
+++ init_window.py
@@ -1,27 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-class MainWindow(QWidget):
+class Window(QWidget):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        #self.main_window = MainWindow()
+        super(Window, self).__init__()
         self.setWindowTitle("Window title")
         self.setGeometry(650,350,500,400)
         
-        self.SecMain()
+        self.Main_Widget()
         self.show()
         
-    def SecMain(self):
+    def Main_Widget(self):
         self.main_widget = QWidget(self)
         self.main_widget.setGeometry(0,0,500,400)
-        #self.main_widget.setStyleSheet('background-color: white;')
-            
+        self.main_widget.setStyleSheet('background-color: white;')
         
+        self.pb_1 = QPushButton('Button 1', self)
+        self.pb_1.setGeometry(10,10,120,50)
         
         
 def StartGUI():
     app = QApplication(sys.argv)
-    temp = MainWindow()
+    temp = Window()
     sys.exit(app.exec_())
 
 if __name__=='__main__':
