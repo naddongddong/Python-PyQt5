7a56f38cee9ac69f401e23f9a8000b35b16cc5c3
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-05-21 15:32:07 +0900

    Add files via upload

---
+++ Reference/01.Create Window/Create_Window.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Main_Window(QWidget):    
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.mainUI()
+    
+    def mainUI(self):
+        self.setWindowTitle("Window Title")
+        self.setFixedSize(500,400)
+        self.setWindowIcon(QIcon('icon.png'))
+        self.show()
+        
+
+def StartGUI():
+    app = QApplication(sys.argv)
+    temp = Main_Window()
+    sys.exit(app.exec_())
+
+if __name__=='__main__':
+    StartGUI()
