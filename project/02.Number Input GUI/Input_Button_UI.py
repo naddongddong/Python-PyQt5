86762c626dd69f8efeba8ef6b4bb069273d9ee68
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-05-31 16:15:40 +0900

    Add files via upload

---
+++ project/02.Number Input GUI/Input_Button_UI.py
@@ -0,0 +1,65 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+        
+class Ui_NumInput(object):
+    def parameter(self):
+        self.background_color = "#A0A0A0"
+        self.button_num_color = "#333333"
+        self.button_func_color = "FF9900"
+        self.button_quit_color = "gray"
+        
+    def setupUi(self, Form):
+        Form.resize(390,450)
+        Form.setFixedSize(390,450)
+        self.num_widget = QWidget(Form)
+        self.num_widget.setGeometry(0,0,390,450)
+        
+        # edit box
+        self.le_num = QLineEdit(self.num_widget)
+        self.le_num.setGeometry(25, 20, 330, 50)
+        
+        self.le_num.setAlignment(Qt.AlignRight)
+        self.le_num.setStyleSheet(
+            'QLineEdit {background-color: #A0A0A0; color: white; border: 1px; border-color: black; border-style: inset;}')
+        self.le_num.setText('')
+        self.le_num.setFont(QFont("Tahoma", 20))
+        
+        self.create_buttons()
+    
+    def create_buttons(self):
+        self.button = [['7','8','9','back'],['4','5','6','clear'],['1','2','3','enter'],['0','0','.','exit']]
+        for i_1, v_1 in enumerate(self.button):
+            for i_2, v_2 in enumerate(v_1):    
+                if i_1 == 3 and i_2 ==  1:
+                    pass
+                else:
+                    self.button[i_1][i_2] = QPushButton(v_2, self.num_widget)
+                    self.button[i_1][i_2].setGeometry(20 + 90 * i_2, 90 * (i_1 + 1), 75, 75)
+                    self.button[i_1][i_2].setStyleSheet('background-color: #333333; color: white; border : 0px; border-radius: 37px;')
+                    self.button[i_1][i_2].clicked.connect(lambda _, a = i_1, b = i_2:self.button_action(a, b))
+        self.button[3][0].setGeometry(20, 360, 160, 75)
+        
+    def button_action(self, i, j):
+        n = self.button[i][j].text()
+        if i<3 and j<3:
+            if self.le_num.text() == '0':
+                self.le_num.setText(str(n))
+            else:
+                self.le_num.setText(self.le_num.text()+str(n))
+        elif i == 3 and j == 0:
+            if self.le_num.text() =='0':
+                pass
+            else:
+                self.le_num.setText(self.le_num.text()[:]+'0')
+        elif i == 0 and j == 3:
+            if self.le_num.text() == '' or len(self.le_num.text()) == 1:
+                self.le_num.setText('0')
+            else:
+                self.le_num.setText(self.le_num.text()[:-1])
+        elif i == 1 and j == 3:
+            self.le_num.setText('0')
+        elif i == 2 and j == 3:
+            print(self.le_num.text())
+            return self.le_num.text()
---
+++ project/02.Number Input GUI/Input_Button_main.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLineEdit, QDialog
+from PyQt5.QtCore import Qt
+from Input_Button_UI import Ui_NumInput
+
+class Main_Window(QWidget):    
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.mainUI()
+        self.numui = Ui_NumInput()
+        
+    def mainUI(self):
+        self.setWindowTitle("Window Title")
+        self.setFixedSize(500,400)
+        
+        self.Edit = QLineEdit("Click to input number",self)
+        self.Edit.setGeometry(160,160,200,30)
+        self.Edit.setAlignment(Qt.AlignCenter)
+        self.Edit.mousePressEvent = self.call_num_input
+        self.show()
+        
+    def call_num_input(self, event):
+        self.input_num_widget = QWidget()
+        self.input_num_widget.setWindowFlags(Qt.FramelessWindowHint)
+        self.input_num_widget.setStyleSheet('background-color: #44546A')
+        self.input_num_widget.setWindowModality(2)
+        
+        self.numui.setupUi(self.input_num_widget)
+        if self.Edit.text() != "Click to input number":
+            self.numui.le_num.setText(self.Edit.text())
+        
+        self.numui.button[3][3].clicked.connect(self.exit)
+        self.numui.button[2][3].clicked.connect(self.enter)
+        
+        self.input_num_widget.show()
+        
+    def exit(self):
+        self.input_num_widget.close()
+    
+    def enter(self):
+        self.Edit.setText(self.numui.le_num.text())
+        self.input_num_widget.close()
+        
+def StartGUI():
+    app = QApplication(sys.argv)
+    temp = Main_Window()
+    sys.exit(app.exec_())
+
+if __name__=='__main__':
+    StartGUI()
