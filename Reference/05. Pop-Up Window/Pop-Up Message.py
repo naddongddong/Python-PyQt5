c5056c15006f102442d9800698e860a5281bcf2b
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-06-02 14:56:54 +0900

    Add files via upload

---
+++ Reference/05. Pop-Up Window/Pop-Up Message.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+
+class Main_Window(QWidget):    
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.mainUI()
+    
+    def mainUI(self):
+        self.setWindowTitle("Window Title")
+        self.setFixedSize(500,400)
+        
+        self.btn = QPushButton("pop-up message",self)
+        self.btn.setGeometry(160,160,200,30)
+        self.btn.clicked.connect(self.popup_message)
+        
+        self.show()
+        
+    def popup_message(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Pop-Up Message")
+        msg.setText("clicked")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
+    
+
+def StartGUI():
+    app = QApplication(sys.argv)
+    temp = Main_Window()
+    sys.exit(app.exec_())
+
+if __name__=='__main__':
+    StartGUI()
