02b95da1946c2821ea85cc3af6696191a527864e
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-06-26 11:45:39 +0900

    Add files via upload

---
+++ Reference/07.StyleSheet/07-2. Label Style/label_style.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+class Main_Window(QWidget):    
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.mainUI()
+    
+    def mainUI(self):
+        self.setWindowTitle("Window Title")
+        self.setFixedSize(500,400)
+        self.setStyleSheet('background-color: white;')
+        
+        self.labels = []
+        for i in range(8):
+            self.labels.append(QLabel(str(i+1)+'번째',self))
+            self.labels[i].setGeometry(50 if i%2 == 0 else 270, 50+80*(i//2), 200, 50)
+            
+        #1. background-color, font-color
+        self.labels[0].setStyleSheet('background-color: #44546A; color: white;')
+
+        #2. align
+        #self.labels[1].setAlignment(Qt.
+        
+        self.show()
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    temp = Main_Window()
+    sys.exit(app.exec_())
