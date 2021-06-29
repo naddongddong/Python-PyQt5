f9f4c53ff0d71b594018841cb6e8fe4b618f6e60
나똥똥 <74886744+naddongddong@users.noreply.github.com>
2021-06-29 16:27:42 +0900

    Add files via upload

---
+++ Reference/09. List widget(리스트 위젯)/LIst_Widget.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QListWidgetItem
+
+class Main_Window(QWidget):    
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.mainUI()
+        self.show()
+    
+    def mainUI(self):
+        self.setFixedSize(300,300)
+        
+        self.listwidget = QListWidget(self)
+        self.listwidget.resize(300, 300)
+        #2
+        self.listwidget.insertItem(0, "Dog")
+        self.listwidget.insertItem(1, "Cat")
+        
+        #2
+        self.listwidget.insertItem(2, QListWidgetItem("Pig"))
+        
+        #3
+        item = QListWidgetItem('Rabbit')
+        self.listwidget.addItem(item)
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    temp = Main_Window()
+    sys.exit(app.exec_())
